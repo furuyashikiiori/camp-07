4ad7f460472def403fc5bd0ba63839ac4e737b41
furuyashikiiori <s2322057@stu.musashino-u.ac.jp>
2025-07-06 18:49:08 +0900

    QRコード生成機能

--- frontend/src/app/qrpage/page.tsx
+++ frontend/src/app/qrpage/page.tsx
@@ -1,13 +1,15 @@
 import Header from "@/components/Header";
 import Footer from "@/components/Footer";
 import styles from "./page.module.css";
+import QRGenerator from "./components/QRGenerator";
 
-export default function QrPage() {
+export default function qrPage() {
   return (
     <div className={styles.container}>
       <Header />
       <main className={styles.main}>
         <h1 className={styles.title}>QRコードページ</h1>
+        <QRGenerator />
         <p>このQRコードをスキャンして、特定の情報にアクセスできます。</p>
       </main>
       <Footer />
