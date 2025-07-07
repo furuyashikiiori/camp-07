d7e80a5a0407f7c9f6d968c1822e580322adcd60
Nozomi Ishimaru <s2322096@stu.musashino-u.ac.jp>
2025-07-07 15:08:01 +0900

    QRページデザイン変更

--- frontend/src/app/qrpage/page.tsx
+++ frontend/src/app/qrpage/page.tsx
@@ -1,21 +1,25 @@
-import Header from "@/components/Header";
-import Footer from "@/components/Footer";
-import styles from "./page.module.css";
-import QRGenerator from "./components/QRGenerator";
+'use client';
 
-export default function qrPage() {
+import QRGenerator from "./components/QRGenerator";
+import Link from 'next/link';
+import styles from "./page.module.css";
+
+export default function QRPage() {
   return (
     <div className={styles.container}>
-      <Header />
       <main className={styles.main}>
+        <Link href="/" className={styles.backLink}>
+          &lt; Back StartPage
+        </Link>
+
         <h1 className={styles.title}>QRコードページ</h1>
-        <QRGenerator />
-        <p>このQRコードをスキャンして、特定の情報にアクセスできます。</p>
-        <p>
-          localhost:3000/mypagenoのURLをQRコードにして表示してる状態
-        </p>
+
+        <div className={styles.card}>
+          <QRGenerator />
+          <p className={styles.paragraph}>このQRコードをスキャンして、特定の情報にアクセスできます。</p>
+          <p className={styles.paragraph}>localhost:3000/mypagenoのURLをQRコードにして表示してる状態</p>
+        </div>
       </main>
-      <Footer />
     </div>
   );
 }
